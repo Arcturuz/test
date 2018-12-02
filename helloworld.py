4a6541006b474b63ca3b195753f674bd9f28f0f5
Arcturuz <daniel.rolfsnes@haugnett.no>
2018-12-02 10:56:05 +0100

    This repo contains README and helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,5 @@
+PyCrust 0.9.8 - The Flakiest Python Shell
+Python 2.7.14+ (default, Mar 13 2018, 15:23:44) 
+[GCC 7.3.0] on linux2
+Type "help", "copyright", "credits" or "license" for more information.
+>>> print("Hello world")
